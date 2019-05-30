c4e75295b420fe02a167b5826e1c14cf762cf85d
Grigoriy Belenkiy <gbelenkiy@361capital.com>
2019-05-30 15:58:48 -0600

    Changing helper comments.

--- code/02-Build/fake-helper.fsx
+++ code/02-Build/fake-helper.fsx
@@ -4,8 +4,6 @@
 #load "./.fake/build.fsx/intellisense.fsx"
 #endif
 
-// When running partial targets (such as BuildTradeUploads.Console)
-// FAKE DSL shows the warning about not matching rules.
-// Yes, but for partial builds the rest is irrelevant, therefore
-// disabling this warning code... --GB
+// When running partial targets FAKE DSL shows the warning about not matching rules.
+// Yes, but for partial builds the rest is irrelevant, therefore disabling this warning code... --GB
 #nowarn "0026"
